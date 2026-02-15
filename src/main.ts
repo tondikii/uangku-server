d74c1b71910d37b4669eba24783a3623cced6a3b
Tondiki Andika <tondikiandika@Tondikis-MacBook-Air.local>
2026-02-15 19:33:28 +0700

    prepare for vercel

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,29 @@
 import { ValidationPipe, HttpStatus } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import serverless from 'serverless-http';
+import * as express from 'express';
+
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+
   app.enableCors();
 
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true, // remove fields unregistered in DTO
-      forbidNonWhitelisted: true, // error if there's any unknown field
+      whitelist: true,
+      forbidNonWhitelisted: true,
       transform: true,
       errorHttpStatusCode: HttpStatus.BAD_REQUEST,
     }),
   );
 
-  await app.listen(process.env.PORT);
+  await app.init();
 }
+
 bootstrap();
+
+export const handler = serverless(server);
